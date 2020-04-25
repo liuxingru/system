27e575a751957347d01163a915181455f51093f7
pxy <3203604400@qq.com>
2020-04-25 09:58:34 +0800

    一部分实体类

---
+++ pregnancy_system/ps-common/src/main/java/com/csdj/pojo/Viability.java
@@ -0,0 +1,25 @@
+package com.csdj.pojo;
+
+/**
+ * 生存能力表
+ */
+public class Viability {
+    private Integer id;//生存能力id
+    private String content;//内容
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
