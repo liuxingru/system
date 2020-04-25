1e8e905acb3496e26cbed0c36441521fb334768a
fyq <1524503229@qq.com>
2020-04-25 18:32:53 +0800

    添加layui

---
+++ pregnancy_system/ps-web/src/main/java/com/csdj/controller/fyq/ToPage.java
@@ -0,0 +1,14 @@
+package com.csdj.controller.fyq;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ToPage {
+    @RequestMapping("/{page}")
+    public String toPage(@PathVariable("page") String page){
+        System.out.println("1111111111111");
+        return page;
+    }
+}
--- pregnancy_system/ps-web/src/main/java/com/csdj/controller/fyq/UltrasonicImageController.java
+++ pregnancy_system/ps-web/src/main/java/com/csdj/controller/fyq/UltrasonicImageController.java
@@ -1,10 +1,13 @@
 package com.csdj.controller.fyq;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.csdj.pojo.UltrasonicImage;
 import com.csdj.service.fyq.UltrasonicImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -13,6 +16,9 @@ import javax.servlet.http.HttpSession;
 public class UltrasonicImageController {
     @Autowired
     private UltrasonicImageService service;
+
+    @RequestMapping(value = "/findUltrasonicImage",produces = {"application/json;charset=utf-8"})
+    @ResponseBody
     public Object findUltrasonicImage(HttpSession session){
        // Record record=(Record) session.getAttribute("User");
         UltrasonicImage ultrasonicImage=new UltrasonicImage();
